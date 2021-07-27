950ef8933c3d802f8c70a8f59570f2f3ef39b7c2
desem <desema.s7@gmail.com>
2021-07-27 12:23:15 +0300

    test fail rev

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -8,6 +8,6 @@ public class Welcomer{
 		return "Farewell, good hunter. May you find your worth in waking world.";
 	}
 	public String sayHuh() {
-		return "Huh, good hurnter. my test string.";
+		return "Huh, good hunter. my test string.";
 	}
 }
